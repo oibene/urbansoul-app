5694e2c48a7ac87579d822656cb9357e43591816
oibene <alvespbene@gmail.com>
2025-10-06 12:19:01 -0300

    adicionando pagina de pesquisa

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import Header from "./pages/header"
 import Footer from "./pages/footer"
 import Index from './pages'
+import ProductSearch from './pages/productSearch';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Index></Index>,
+    },
+    {
+      path: "/search",
+      element: <ProductSearch></ProductSearch>,
+    },
+    
+  ]
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Header></Header>
-    <Index></Index>
-    <Footer></Footer>
+    <div className="bg-bglight">
+      <Header></Header>
+      <RouterProvider router={router} />
+      <Footer></Footer>
+
+    </div>
   </StrictMode>,
 )
